6692be57747185d7228dbfb954d15947d9a125d9
alexktrinh <alexkltrinh@gmail.com>
2023-07-27 12:29:41 -0400

    Update chess bot

--- Programming/ChessBot.c
+++ Programming/ChessBot.c
@@ -11,7 +11,7 @@ D: x
 const int X_COORD[8] ={4400,8300,13575,18150,22950,27200,32000,37000};
 const int Y_COORD[8] = {14280,12350,10430,8500,6150,4250,1960,80}; //subject to change due to y-zeroing
 
-const int zeroDist = 6;
+const int X_ZERO_DIST = 6;
 const tSensors X_ZERO = S4;
 const tSensors Y_ZERO = S2;
 const tSensors Z_ZERO = S3;
@@ -24,25 +24,28 @@ void configureAllSensors()
 	wait1Msec(100);
 }
 
-void zeroAllMotors()
+bool zeroAllMotors()
 {
 	motor[motorA] = -100;
 	motor[motorB] = -70;
 	motor[motorD] = -100;
 
-	while (!(SensorValue[Z_ZERO] == 1 && SensorValue[Y_ZERO] == 1 && SensorValue[X_ZERO] < zeroDist)) //change "2" to appropriate value.
+	while (!(SensorValue[Z_ZERO] == 1 && SensorValue[Y_ZERO] == 1 && SensorValue[X_ZERO] < X_ZERO_DIST)) //change "2" to appropriate value.
 	{
 		if(SensorValue[Z_ZERO] == 1)
 			motor[motorB] = 0;
 		if(SensorValue[Y_ZERO] == 1)
 			motor[motorA] = -0.5;
-		if(SensorValue[X_ZERO] < zeroDist)
+		if(SensorValue[X_ZERO] < X_ZERO_DIST)
 			motor[motorD] = 0;
-		if(SensorValue[X_ZERO] < 10 && !(SensorValue[X_ZERO] < zeroDist))
+		if(SensorValue[X_ZERO] < 10 && !(SensorValue[X_ZERO] < X_ZERO_DIST))
 			motor[motorD] = -75;
+		if(getButtonPress(buttonAny) == 1)
+			return false;
 	}
 	motor[motorA] = motor[motorB] = motor[motorD] = 0;
 	nMotorEncoder[motorA] = nMotorEncoder[motorB] = nMotorEncoder[motorD] = 0;
+	return true;
 }
 
 
@@ -68,13 +71,18 @@ bool moveXY(int x, int y) //a1 is (0,0), will take a bool value and return 1 nor
 	return true;
 }
 
-bool moveFromXY(int x, int y, int x2, int y2)
+bool moveFromXY(int x, int y)
 {
 	//D=x-axis A=y-axis
-	nMotorEncoder[motorD]= nMotorEncoder[motorA] = 0;
 
-	int distX = Y_COORD[y2] - Y_COORD[y];
-	int distY = X_COORD[x2] - X_COORD[x];
+
+	//int distX = Y_COORD[y2] - Y_COORD[y];
+	//int distY = X_COORD[x2] - X_COORD[x];
+	int initDistX = nMotorEncoder[motorD];
+	int initDistY = nMotorEncoder[motorA];
+
+	int distX = X_COORD[x] - initDistX;
+	int distY = Y_COORD[y] - initDistY;
 
 	if(distX > 0)
 		motor[motorD] = 75;
@@ -86,17 +94,16 @@ bool moveFromXY(int x, int y, int x2, int y2)
 	else
 		motor[motorA] = -75;
 
-
-
-	while (fabs(nMotorEncoder[motorD]) < fabs(distX) || fabs(nMotorEncoder[motorA]) < fabs(distY))
+	while (abs(nMotorEncoder[motorD] - initDistX) < abs(distX) || abs(nMotorEncoder[motorA]-initDistY) < abs(distY))
 	{
-		if(nMotorEncoder[motorD] > fabs(distX))
+		if(abs(nMotorEncoder[motorD] - initDistX) > abs(distX))
 			motor[motorD] = 0;
-		if(nMotorEncoder[motorA] > fabs(distY))
+		if(abs(nMotorEncoder[motorA]-initDistY) > abs(distY))
 			motor[motorA] = 0;
 		if(getButtonPress(buttonAny) == 1)
 			return false;
 	}
+
 	motor[motorD] = motor[motorA] = 0;
 	return true;
 
@@ -138,14 +145,7 @@ bool pickUpPiece() // same return as moveXY Calum
 	nMotorEncoder[motorC] = 0;
 	wait1Msec(2000);
 	motor[motorC] = 10;
-	/*
-	while(nMotorEncoder[motorC] < 150)
-	{
-		if(getButtonPress(buttonAny) == 1)
-			return false;
-	}
-	*/
-	wait1Msec(5000);
+	wait1Msec(2500);
 	motor[motorB] = -40;
 	while(SensorValue[Z_ZERO] == 0)
 	{
@@ -156,18 +156,25 @@ bool pickUpPiece() // same return as moveXY Calum
 	return true;
 }
 
-void dropPiece()
+bool dropPiece()
 {
 	motor[motorB] = 50;
 	while(nMotorEncoder[motorB] < 1100 )
-	{}
+	{
+		if(getButtonPress(buttonAny) == 1)
+			return false;
+	}
 	motor[motorB]=0;
 	wait1Msec(2000);
 	motor[motorC] = -10;
 	while(nMotorEncoder[motorC] > 0)
-	{}
+	{
+		if(getButtonPress(buttonAny) == 1)
+			return false;
+	}
 	motor[motorC] = 0;
 
+	return true;
 }
 
 bool removePiece(int &counter) //same return as moveXY
@@ -178,9 +185,9 @@ bool removePiece(int &counter) //same return as moveXY
 	motor[motorA] = 100;
 	motor[motorD]= -30;
 
-	while (nMotorEncoder[motorA] < y || SensorValue[X_ZERO] < 5)
+	while (nMotorEncoder[motorA] < y || SensorValue[X_ZERO] > X_ZERO_DIST)
 	{
-		if(SensorValue[X_ZERO] > 5)
+		if(SensorValue[X_ZERO] < X_ZERO_DIST)
 			motor[motorD] = 0;
 		if(nMotorEncoder[motorA] > y)
 			motor[motorA] = 0;
@@ -202,9 +209,9 @@ bool removePiece(int &counter) //same return as moveXY
 
 int chessMoves(TFileHandle & fin)//reads in the files and generates move
 {
-	int numMoves = 0, statPiece = 10, x = 0, y = 0, x2 = 0, y2 = 0;
-	int counter = 0;
+	int numMoves = 0, statPiece = 10, x = 0, y = 0, x2 = 0, y2 = 0, counter = 0;
 	string startColor, piece;
+	bool movePiece = true;
 
 	/*
 	if(removepiece || pickUpPiece || dropPiece == !true )
@@ -215,49 +222,49 @@ int chessMoves(TFileHandle & fin)//reads in the files and generates move
 
 	while(readTextPC(fin, piece) && readIntPC(fin, statPiece))
 	{
-		if (piece == "Checkmate")
-		{
-			displayString(8, "Game Over!");
-			return numMoves;
-		}
-		displayString(9, "Status: %d", statPiece);
 		numMoves+=1;
+
+		displayString(9, "Status: %d", statPiece);
 		readIntPC(fin, y);
 		readIntPC(fin, x);
 		readIntPC(fin, y2);
 		readIntPC(fin , x2);
 
-		displayString(10, "Move %s from %d,%d", piece ,x,y);
-		displayString(11,"to %d, %d",x2,y2);
+		displayString(10, "Move %s from %d,%d to %d,%d", piece ,x,y,x2,y2);
 		zeroAllMotors();
 
 		if(statPiece == 0)
 		{
-			moveXY(x,y);
-			pickUpPiece();
+			//movePiece = moveXY(x,y)&& pickUpPiece()&&zeroAllMotors()&&moveXY(x2,y2) && dropPiece();
+			movePiece = moveXY(x,y)&& pickUpPiece()&& moveFromXY(x2,y2) && dropPiece();
+			if(!movePiece)
+			{
+				return numMoves;
+			}
 
-			moveFromXY(x,y,x2,y2);
-			//zeroAllMotors();
-			//moveXY(x2,y2);
-			dropPiece();
 		}
 		else
 		{
-			moveXY(x2,y2);
-			pickUpPiece();
-			removePiece(counter);
-			zeroAllMotors();
-			moveXY(x,y);
-			pickUpPiece();
-			//zeroAllMotors();
-			moveFromXY(x,y,x2,y2);
-			dropPiece();
+			/*movePiece = moveXY(x2,y2) && pickUpPiece() && removePiece(counter) && zeroAllMotors()
+									&& moveXY(x,y) && pickUpPiece()&& zeroAllMotors() && moveXY(x2,y2)
+									&& dropPiece();*/
+			movePiece = moveXY(x2,y2) && pickUpPiece() && removePiece(counter) && zeroAllMotors()
+									&& moveXY(x,y) && pickUpPiece()&& moveFromXY(x2,y2)
+									&& dropPiece();
+
+			//moveFromXY(x,y,x2,y2);
+			if(!movePiece)
+			{
+				return numMoves;
+			}
 		}
 	}
 
-	if(piece!="")
-		displayString(14, "%s", piece);
-
+	if (piece == "Checkmate")
+	{
+		displayString(8, "Game Over!");
+		return numMoves;
+	}
 
 	return numMoves;
 }
@@ -269,8 +276,7 @@ task main()
 	int movesPlayed = 0;
 	configureAllSensors();
 
-	//callibrateBoard();
-	//zeroAllMotors();
+	callibrateBoard();
 
 	displayString(5,"Press Any Button To Start!");		//initialize game
 	while(!getButtonPress(buttonAny))
@@ -282,7 +288,7 @@ task main()
 	TFileHandle fin;
 	bool fileCheck = openReadPC(fin , "Chess_Game_1.txt");
 
-	/*
+
 
 	if(!fileCheck)
 	{
@@ -292,11 +298,12 @@ task main()
 	{
 		movesPlayed = chessMoves(fin);
 		int time = time1[T1] / 1000;
-		displayString(15, "Game finished in %d moves and %\n in %d seconds.", movesPlayed, time);
-	}*/
+		displayString(10, "Game finished in %d moves and in %d seconds.", movesPlayed, time);
+	}
+	/*
 	zeroAllMotors();
 	moveXY(0,0);
 	pickUpPiece();
 	moveFromXY(0,0,1,2);
-	dropPiece();
+	dropPiece();*/
 }
